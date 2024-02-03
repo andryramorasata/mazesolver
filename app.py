5b91a5c2bcd09c7ace62ecd9d3416e974de36741
andryramorasata <andryramorasata@gmail.com>
2024-02-03 14:31:23 -0500

    render window

--- app.py
+++ app.py
@@ -3,23 +3,26 @@ from tkinter import Tk, BOTH, Canvas
 class Window:
     def __init__(self, width, height):
         self.root = Tk()
-        root.title("My Maze Solver")
-        canvas = Canvas(self.root, {"width":width, "height":height})
-        canvas.pack()
-        isRunning = False
+        self.root.title("My Maze Solver")
+        self.canvas = Canvas(self.root, {"width":width, "height":height})
+        self.canvas.pack(fill="both", expand = True)
+        self.isRunning = False
         self.root.protocol("WM_DELETE_WINDOW", self.close)
 
-    def redraw():
+    def redraw(self):
         self.root.update_idletasks()
         self.root.update()
     
-    def wait_for_close():
+    def wait_for_close(self):
+        self.isRunning = True
         while self.isRunning:
             self.redraw()
 
-    def close():
+    def close(self):
         self.isRunning = False
 
 def main():
     win = Window(800, 600)
     win.wait_for_close()
+
+main()
