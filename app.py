fc54240e437b87235b2537cac4d9cd04daea3e2c
andryramorasata <andryramorasata@gmail.com>
2024-02-04 13:31:30 -0500

    add draw line

--- app.py
+++ app.py
@@ -13,6 +13,9 @@ class Window:
         self.root.update_idletasks()
         self.root.update()
     
+    def draw_line(self,line, fill_color):
+        line.draw(self.canvas, fill_color)
+
     def wait_for_close(self):
         self.isRunning = True
         while self.isRunning:
@@ -21,8 +24,28 @@ class Window:
     def close(self):
         self.isRunning = False
 
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+class Line:
+    def __init__(self, point1, point2):
+        self.point1 = point1
+        self.point2 = point2
+
+    def draw(self,canvas, fill_color):
+        canvas.create_line(
+            self.point1.x, self.point1.y, 
+            self.point2.x, self.point2.y,
+            fill=fill_color,
+            width=2
+        )
+        canvas.pack(fill="both", expand=True)
+
 def main():
     win = Window(800, 600)
+    win.draw_line(Line(Point(0,0), Point(800,600)), "red")
     win.wait_for_close()
 
 main()
