59a6993f34a853f979297351ee2b0e6667a28e2e
andryramorasata <andryramorasata@gmail.com>
2024-02-06 08:42:57 -0500

    refactor geometry out

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from tkinter import Tk, BOTH, Canvas
+from geometry import Point, Line
 
 class Window:
     def __init__(self, width, height):
@@ -23,25 +24,6 @@ class Window:
 
     def close(self):
         self.isRunning = False
-
-class Point:
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
-
-class Line:
-    def __init__(self, point1, point2):
-        self.point1 = point1
-        self.point2 = point2
-
-    def draw(self,canvas, fill_color):
-        canvas.create_line(
-            self.point1.x, self.point1.y, 
-            self.point2.x, self.point2.y,
-            fill=fill_color,
-            width=2
-        )
-        canvas.pack(fill="both", expand=True)
 
 def main():
     win = Window(800, 600)
